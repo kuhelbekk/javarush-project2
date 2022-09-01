728525931d28dc0bdd2759444d1af8932ce93648
Ivlev Aleksey <kuhelbekk@gmail.com>
2022-09-01 03:05:05 +0300

    refactor

--- src/ru/javarush/ivlev/module2/island/WaterCell.java
+++
@@ -1,9 +0,0 @@
-package ru.javarush.ivlev.module2.island;
-
-import java.util.List;
-
-public class WaterCell extends Cell {
-    public WaterCell(Island parent, List<AnimalType> animaltypes, int posX, int posY) {
-        super(parent, posX, posY);
-    }
-}
